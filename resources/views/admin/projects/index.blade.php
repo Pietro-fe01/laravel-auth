8b50bbab6a01b0925c79145de960ecbdaf043677
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-25 14:51:40 +0100

    Style tabella: - resa responsive - contenuto allineato al centro dlla propria colonna

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,26 +2,28 @@
 
 @section('content')
     <h1 class="my-3">Projects List</h1>
-    <table class="table table-bordered table-hover">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Project title</th>
-                <th scope="col">Customer name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Slug</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($projects as $project)
-            <tr>
-                <td>{{ $project->id }}</td>
-                <td>{{ $project->project_title }}</td>
-                <td>{{ $project->customer_name }}</td>
-                <td>{{ $project->description }}</td>
-                <td>{{ $project->slug }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-bordered table-hover align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Project title</th>
+                    <th scope="col">Customer name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Slug</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->project_title }}</td>
+                    <td>{{ $project->customer_name }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>{{ $project->slug }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
